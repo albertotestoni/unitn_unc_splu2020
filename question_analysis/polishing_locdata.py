194a17f2d159e74a329bec8a5df884f0dac72953
maury_green <mauriciomazuecos@pm.me>
2020-07-02 12:55:50 -0300

    Corrected is_true_absolute function.

--- question_analysis/polishing_locdata.py
+++ question_analysis/polishing_locdata.py
@@ -2,16 +2,16 @@ import pandas as pd
 
 # Run this to filter out absolute questions with more than
 # <spatial> in their category
-locdata = pd.read_csv('locationq_classification.csv')
-locdata['cat'] = locdata['cat'].astype(str)
-cond = ~(locdata['location_type'] == 'absolute') | (locdata['cat'] == "['<spatial>']")
-newlocdata = locdata[cond]
-newlocdata.to_csv('locdata_with_absolute_filtered.csv', index=False)
+#locdata = pd.read_csv('locationq_classification.csv')
+#locdata['cat'] = locdata['cat'].astype(str)
+#cond = ~(locdata['location_type'] == 'absolute') | (locdata['cat'] == "['<spatial>']")
+#newlocdata = locdata[cond]
+#newlocdata.to_csv('locdata_with_absolute_filtered.csv', index=False)
 
-
+newlocdata = pd.read_csv('./locdata_with_absolute_filtered.csv')
 # Run this to classify the remaining absolute questions between
 # true absolute and non true absolute (kinda weird name)
-absoluteonly = newlocdata[ newlocdata['location_question'] == 'absolute']
+absoluteonly = newlocdata[ newlocdata['location_type'] == 'absolute']
 
 superlatives = """rightmost 
  leftmost 
@@ -49,13 +49,10 @@ rest = rest.split('\n')
 def is_true_absolute(q):
     words = q.split()
     end_pos = len(words)
-    for kw in superlatives:
-        # The superlative words (leftmost, rightmost, etc) are absolute no matter where they appear.
-        if kw in q:
+    for i, w in enumerate(words):
+        if w in superlatives:
             return True
-    for kw in rest:
-        # The rest has to appear before the "?" or before "one ?
-        if kw == words[-2]:
+        if w in rest and i < end_pos:
             return True
     return False
 
